df7a5e4696c09ae67f54d5381ec75890f2328864
digitapex <spitslide@gmail.com>
2018-10-13 15:32:25 +0200

    Add toolbar in item activity

--- app/src/main/java/com/spitslide/ukpetitions/PetitionItemActivity.java
+++ app/src/main/java/com/spitslide/ukpetitions/PetitionItemActivity.java
@@ -2,10 +2,11 @@ package com.spitslide.ukpetitions;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
-
-import java.io.Serializable;
 
 
 public class PetitionItemActivity extends AppCompatActivity {
@@ -15,5 +16,19 @@ public class PetitionItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_petitions_item);
         PetitionItem petitionItem = (PetitionItem)getIntent().getSerializableExtra("PetitionItem");
         ((TextView)findViewById(R.id.petition_title)).setText(petitionItem.getTitle());
+        Toolbar toolbar = findViewById(R.id.toolbar_item);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
