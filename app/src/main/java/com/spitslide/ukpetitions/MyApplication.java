a4433cd369881684da6307ae4dd7e51ed7be1384
digitapex <spitslide@gmail.com>
2019-08-15 23:27:52 +0200

    Fix connection issue for Kitkat

---
+++ app/src/main/java/com/spitslide/ukpetitions/MyApplication.java
@@ -0,0 +1,33 @@
+package com.spitslide.ukpetitions;
+
+
+import android.app.Application;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // needed for pre-Lollipop devices
+        // https://stackoverflow.com/a/51285550/9702500
+        try {
+            ProviderInstaller.installIfNeeded(getApplicationContext());
+            SSLContext sslContext;
+            sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, null, null);
+            sslContext.createSSLEngine();
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException
+                | NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
+}
